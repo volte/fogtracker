552bb823e6e01310845abadb45c2d10656c79e37
volte <100103+volte@users.noreply.github.com>
2024-12-06 18:41:39 -0500

    Configuration updates

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  snapshotResolver: '<rootDir>/jest/SnapshotResolver.ts',
+};
+
+export default config;
---
+++ jest/SnapshotResolver.ts
@@ -0,0 +1,17 @@
+const snapshotResolver = {
+  resolveSnapshotPath: (testPath: string, snapshotExtension: string) => {
+    // Resolve the snapshot path to the corresponding path under the snapshots directory
+    return testPath.replace('src/', 'jest/snapshots/') + snapshotExtension;
+  },
+
+  resolveTestPath: (snapshotFilePath: string, snapshotExtension: string) => {
+    // Resolve the test path to the corresponding path under the src directory
+    return snapshotFilePath
+      .replace('snapshots/', 'src/')
+      .slice(0, -snapshotExtension.length);
+  },
+
+  testPathForConsistencyCheck: 'src/folder1/folder2/example.test.ts',
+};
+
+export default snapshotResolver;
