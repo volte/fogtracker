b7dcb3fba958d8ef816d16f0f01b8fe06f7a1fd8
volte <100103+volte@users.noreply.github.com>
2024-12-06 18:41:40 -0500

    Reorg

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 const AppContainer = styled.div``;
 
+/**
+ * Root app component.
+ */
 export default function App() {
   return <AppContainer>foo</AppContainer>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from '@/App';
+import App from '@/components/App';
 
 import '@assets/css/index.css';
 import '@assets/css/reset.css';
