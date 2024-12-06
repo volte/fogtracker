e34ad5d0100250d635a71ae9af1668f4fc4e07af
volte <100103+volte@users.noreply.github.com>
2024-12-06 18:41:39 -0500

    Config updates again

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import * as React from 'react';
 import styled from 'styled-components';
 
 const AppContainer = styled.div``;
 
-function App() {
+export default function App() {
   return <AppContainer>foo</AppContainer>;
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 
-import './index.scss';
+import '@assets/css/index.css';
+import '@assets/css/reset.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
